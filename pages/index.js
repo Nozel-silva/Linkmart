a636b3f1282797ce1a18f75e597fac3326f78060
Nuel Agafie <70712577+Nozel-silva@users.noreply.github.com>
2025-08-04 20:37:42 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { ethers } from 'ethers';
-import contractABI from '../contractABI.json';
+import contractABI from 'Linkmart/contractABI.json';
 
 const contractAddress = '0xc979F91746132cA63C027AbfD04273fbE2ad4501';
 
