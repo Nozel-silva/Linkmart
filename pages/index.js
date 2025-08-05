282c880f35cb74c8a1878a03c6099b7a5fccb8e9
Nuel Agafie <70712577+Nozel-silva@users.noreply.github.com>
2025-08-05 02:31:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { ethers } from 'ethers';
 import contractABI from '../contractABI.json'; 
+const contractABI =
 
+
+  
 const contractAddress = '0xc979F91746132cA63C027AbfD04273fbE2ad4501';
 
 export default function Home() {
